02ae783cbba548e58da439894d2a0e455b9de3b9
Charlene2002 <78091102+Charlene2002@users.noreply.github.com>
2026-06-07 13:42:51 +0200

    Update TransformationTableBleu.py

--- python/TransformationTableBleu.py
+++ python/TransformationTableBleu.py
@@ -6,20 +6,28 @@ from bq_utils import charger_dataframe_vers_bigquery
 def transformer_polluants():
     dossier_entree = Path("/tmp/data")
 
+
     df = pd.read_csv(dossier_entree / "polluants.csv")
 
-    df_polluants = df[["codeue", "code", "notation", "libelle_fr", "code_unite_concentration"]].drop_duplicates()
-    df_polluants = df_polluants.rename(columns={
-        "codeue": "id_poll_ue",
-        "notation":"notation",
-        "code": "code_poll",
-        "libelle_fr": "nom_poll",
-        "code_unite_concentration": "unite"
-    })
-    df_polluants = df_polluants.dropna(subset=["id_poll_ue"])
+    # Convertir la colonne code en nombre
+    df["code"] = pd.to_numeric(df["code"], errors="coerce")
 
-    print(f"{len(df_polluants)} polluants récupérés")
-    charger_dataframe_vers_bigquery(df_polluants, "dim_polluants", mode_ecrasement=True)
+    codes_polluants = [8, 24, 39, 3, 1]
+
+    df_polluants = (
+        df.loc[
+            df["code"].isin(codes_polluants),
+            ["code", "notation", "code_unite_concentration"]
+        ]
+        .drop_duplicates()
+        .rename(columns={
+            "code": "code_poll",
+            "code_unite_concentration": "unite"
+        })
+        .sort_values("code_poll")
+    )
+
+    df_polluants["code_poll"] = df_polluants["code_poll"].astype(int)
 
 
 if __name__ == "__main__":
